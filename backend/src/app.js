9e29100fe54ed725a3cefbf86f71b91929b21131
Sharan Reddy <sharansaikonda99@gmail.com>
2021-01-23 08:19:19 +0530

    testing

--- backend/src/app.js
+++ backend/src/app.js
@@ -17,9 +17,9 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static(ProjectURL));
 
-app.use("/api", directMessagesRouter);
-app.use("/api", usersRouter);
-app.use("/api", emailRouter);
+app.use("", directMessagesRouter);
+app.use("", usersRouter);
+app.use("", emailRouter);
 
 app.get("*", function (req, res) {
   res.sendFile(path.join(ProjectURL + "/index.html"));
