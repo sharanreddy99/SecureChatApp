dc832897dbb9c15513ea7d830b38ab78454aa876
Sharan Reddy <sharansaikonda99@gmail.com>
2021-04-27 09:36:53 +0530

    update environment variables

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,13 @@ import firebase from "firebase";
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCm06l81PepowAEpiXwNjGsyV5BaZOewQk",
-  authDomain: "encryptchat-e5dda.firebaseapp.com",
-  projectId: "encryptchat-e5dda",
-  storageBucket: "encryptchat-e5dda.appspot.com",
-  messagingSenderId: "112975287816",
-  appId: "1:112975287816:web:ac19f1c27174776062e3bd",
-  measurementId: "G-ZVPJSQZ498",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
