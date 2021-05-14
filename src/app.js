fa225da605b6fd788491dfbfc92d6c29ef8d48a1
Sharan Reddy <sharansaikonda99@gmail.com>
2021-05-14 14:45:09 +0530

    update chat

--- src/app.js
+++ src/app.js
@@ -3,8 +3,8 @@ const cors = require("cors");
 require("dotenv").config();
 require("./db/mongoose");
 
-const path = require("path");
-const ProjectURL = path.join(__dirname, "Securechats");
+// const path = require("path");
+// const ProjectURL = path.join(__dirname, "Securechats");
 
 const directMessagesRouter = require("./routers/directmessages");
 const groupMessagesRouter = require("./routers/groupmessages");
@@ -21,16 +21,16 @@ app.set("socketio", io);
 
 require("./routers/automaticserver")(io);
 
-app.use(express.static(ProjectURL));
+// app.use(express.static(ProjectURL));
 
 app.use("/api", directMessagesRouter);
 app.use("/api", groupMessagesRouter);
 app.use("/api", usersRouter);
 app.use("/api", emailRouter);
 
-app.get("*", function (req, res) {
-  res.sendFile(path.join(ProjectURL + "/index.html"));
-});
+// app.get("*", function (req, res) {
+//   res.sendFile(path.join(ProjectURL + "/index.html"));
+// });
 
 io.on("connection", (client) => {
   console.log("client: ", client.id);
--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-mongoose.connect(process.env.MONGOURL, {
+mongoose.connect(process.env.MONGOURL_LOCAL, {
   useCreateIndex: true,
   useNewUrlParser: true,
   useUnifiedTopology: true,
